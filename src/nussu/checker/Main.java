82606e90cc7a25e0ce6a5686421753ca24fe8ba3
Chan Jun Wei <chanjunweimy@gmail.com>
2016-09-15 02:40:22 +0800

    a simple gui is out
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- src/nussu/checker/Main.java
+++ src/nussu/checker/Main.java
@@ -1,25 +1,79 @@
 package nussu.checker;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private Stage _primaryStage;
+	private BorderPane _rootLayout;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		initApplicationPrimaryStage(primaryStage);
+		initRootLayout();
+		showCheckerInterface();
 	}
 	
+	private void initApplicationPrimaryStage(Stage primaryStage) {
+		_primaryStage = primaryStage;
+		_primaryStage.setTitle("Duplicate Checker");
+	}
+	
+	/**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+    	_rootLayout = null;
+    	try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            _rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(_rootLayout);
+            scene.getStylesheets().add(getClass().getResource("style/application.css").toExternalForm());
+            _primaryStage.setScene(scene);
+            _primaryStage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    
+    /**
+     * Shows the checker interface inside the root layout.
+     */
+    public void showCheckerInterface() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/CheckerInterface.fxml"));
+            AnchorPane checkerInterface = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            _rootLayout.setCenter(checkerInterface);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return _primaryStage;
+    }
+    
 	public static void main(String[] args) {
 		launch(args);
 	}
